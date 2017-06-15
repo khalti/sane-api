4e23191fc8a354109333fc13367bb2d254e9e041
ludbek <sth.srn@gmail.com>
2017-06-15 10:52:13 +0545

    - added tmux at .brotasks - added editorconfig

--- .brotasks/default.sh
+++ .brotasks/default.sh
@@ -3,8 +3,24 @@
 project=$(basename `pwd`)
 
 init () {
-  source .env/bin/activate
-  echo "Happy hacking !!!"
+	bro setup_tmux
+	echo "Happy hacking !!!"
+}
+
+env () {
+	source .env/bin/activate
+}
+
+setup_tmux () {
+	structure $project
+	window editor
+		run "bro env"
+		run "nvim"
+	window terminal
+		run "bro env"
+
+	focus editor
+	connect $project
 }
 
 $@
