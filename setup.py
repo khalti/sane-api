b79107b2a5b9a226acf31857cdb70f3993511743
ludbek <sth.srn@gmail.com>
2017-07-25 17:26:22 +0545

    added license, readme and setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+import json
+
+# https://docs.python.org/2/distutils/setupscript.html
+
+def read_file(filename):
+	f = open(filename)
+	data = f.read()
+	f.close
+	return data
+
+kwargs =  {
+  "name": "sane-api",
+  "version": "0.1.0",
+  "description": "Secure and scalable REST API.",
+  "long_description": read_file("readme.md"),
+  "author": "ludbek"
+  "author_email": "sth.srn@gmail.com",
+  "url": "https://github.com/janakitech/sane-api",
+  "license": read_file("license.txt"),
+  "keywords": "api rest django drf safe scalable",
+  "packages": find_packages(exclude=["tests"]),
+  "install_requires": json.loads(read_file("project.json"))["dependencies"],
+}
+
+setup(**kwargs)
