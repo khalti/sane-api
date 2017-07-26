33c246c2a84ffe850bcdb53304ab1629b6b22bcb
ludbek <sth.srn@gmail.com>
2017-07-26 12:04:01 +0545

    data tree starts at article

--- examples/blog/app/serializers.py
+++ examples/blog/app/serializers.py
@@ -1,22 +1,21 @@
 from sane_api.serializers import SaneModelSerializer
 from app.models import Article, Comment
 
+class CommentSerializer(SaneModelSerializer):
+	def get_readable_fields(self):
+		return ["content", "article", "user", "created_on", "modified_on", "id"]
+
+	class Meta:
+		model = Comment
+		fields = "__all__"
 
 class ArticleSerializer(SaneModelSerializer):
+	comments = CommentSerializer(many=True)
+
 	def get_readable_fields(self):
-		return ["title", "body", "user", "created_on", "modified_on"]
+		return ["title", "body", "user", "created_on", "modified_on", "comments"]
 
 	class Meta:
 		model = Article
 		fields = "__all__"
 
-
-class CommentSerializer(SaneModelSerializer):
-	article = ArticleSerializer()
-
-	def get_readable_fields(self):
-		return ["content", "article", "user", "created_on", "modified_on"]
-
-	class Meta:
-		model = Comment
-		fields = "__all__"
